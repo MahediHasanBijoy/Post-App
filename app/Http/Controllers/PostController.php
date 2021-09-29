2b10752d015e13ba57915b5d6a67cf763fef8ae3
MahediHasanBijoy <bijoycse2014@gmail.com>
2021-09-30 01:37:03 +0600

    project end...

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,6 +14,16 @@ class PostController extends Controller
 
     	return view('posts.index',['posts'=>$posts]);
     }
+
+    public function show(Post $post)
+    {
+        return view ('posts.show',[
+        'post'=>$post]);
+    }
+
+
+
+
     public function store(Request $request)
     {
     	//validate
--- resources/views/users/posts/index.blade.php
+++ resources/views/users/posts/index.blade.php
@@ -4,30 +4,15 @@
 @section('content')
 
 <div class="flex justify-center">
-	<div class="w-8/12 bg-white p-6 rounded-lg">
-		@auth
-		<form action="{{ route('posts') }}" method="post" class="mb-4">
-			@csrf
-			<div class="mb-4">
-				<label for="body" class="sr-only">Body</label>
-				<textarea name="body" id="body" cols="30" rows="4" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('body') border-red-500 @enderror " placeholder="Post something"></textarea>	
-				@error('body')
-				<div class="text-red-500 mt-2 text-sm">
-					{{$message}}
-				</div>
-				@enderror		
-			</div>
+	<div class="w-8/12">
+		<div class="p-6">
+			<h1 class="text-2xl font-medium mb-1">{{$user->name}} </h1>
+			<p>Posted {{$posts->count()}} {{Str::plural('post', $posts->count())}} </p>
+		</div>
 
-			<div>
-				<button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded font-medium ">Post</button>
-			</div>
-
-
-		</form>
-		@endauth
-
-		@if($posts->count())
-			@foreach($posts as $post)
+		<div class=" bg-white p-6 rounded-lg">
+			@if($posts->count())
+				@foreach($posts as $post)
 				<div class="mb-4">
 					<a href="{{route('users.posts',$post->user)}} " class="font-bold">{{$post->user->name}}</a> <span class="text-gray-600 text-sm">{{$post->created_at->diffForHumans()}}</span>
 					<p class="mb-2">{{$post->body}}</p>
@@ -66,11 +51,17 @@
 
 
 				</div>
-			@endforeach
+				@endforeach
 			{{ $posts->links() }} 
-		@else
-			{{"There is no posts"}}
-		@endif
+			@else
+				<p>{{$user->name}} does not have any posts</p>
+			@endif
+
+		</div>
+		
+
+
+
 	</div>
 </div>
 
